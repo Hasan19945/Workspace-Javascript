a85ce682ddad94bbfbfe9c5d4037bec6b425bdb0
hasan <hasancilenger1773@gmail.com>
2023-10-18 19:05:14 +0200

    for-while

--- javascript/class-notes/odevler/array.js
+++ javascript/class-notes/odevler/array.js
@@ -1,4 +1,4 @@
-var a = ['dog','lion','hen'];
+/*var a = ['dog','lion','hen'];
 a[100] = 'horse';
 var b = ['ali', 'veli', 'deli'];
 b[100] = 'neki'
@@ -9,11 +9,19 @@ let array = [1, 2, 3];
 array[6] = 9;
 console.log(array[5]); //undifined
 
-let myArray = [1, 2, 3, 4, 5];
-myArray[2]=6
-console.log(myArray);// [1,,2,6,4,5] seklinde oldu
 
-console.log(myArray.length);
+
+
+
+
+
+
+
+
+
+
+
+//console.log(myArray.length);
 for (var i = 0;  i < 5; i++){
     console.log("i degeri:" +i);
 }
@@ -100,3 +108,71 @@ var calisan={
 }
 
  console.log(calisan.adi);
+
+ /*let myArray = [1, 2, 3, 4, 5];
+myArray[6]=6
+console.log(myArray);// [1,,2,6,4,5] seklinde oldu
+console.log(myArray[19]);
+myArray.unshift(0)
+console.log(myArray);
+myArray.push(1,24)
+console.log(myArray);
+myArray.splice(1,2,6)
+console.log(myArray);
+myArray.reverse()
+console.log(myArray);
+delete myArray[0]
+console.log(myArray);
+let myArra = [1, 2, 3, 4, 5];
+myArra[2]
+console.log(myArra); // [1, 2, 6, 4, 5]*/
+
+/*let str1="listen"
+let str2='slient'
+function areAnagrams () {
+    if (str1.length !== str2.length) {
+        console.log("bu bir anagram degildir!");
+        return false
+    } else {
+        
+    }
+}*/
+for (let i = 0; i < 5; i++) {
+    console.log("Döngü adımı " + i);
+  }
+for (let k=0; k<4; k++) {
+    console.log("k dongusu deneme:" +  2*k);
+}
+
+for (let j=0;  j<5; j++) {
+console.log('j donusu:' + (j+j));
+}
+
+{let i = 0;
+
+while (i < 5) {
+  console.log("Döngü adımı " + i);
+  i++;
+}
+}
+let k =0;
+while (k<5) {
+    console.log("dongu k:" +k);
+    k++;
+}
+let j = 0;
+while (j < 3) {
+    console.log("j dongusu:" + j+j );
+    j++;
+}
+/*for (;;) {
+  // sonsuz döngü
+}
+for (let i = 0; i < 10; i++) {
+
+  // Eğer 2'ye bölünebiliyorsa bir sonraki adıma atlar.
+  if (i % 2 == 0) continue;
+
+  alert(i); // ekranda 1, 3, 5, 7, 9 değerleri gösterilir.
+}
+*/
---
+++ javascript/class-notes/odevler/yeniarray.js
@@ -0,0 +1,42 @@
+// 1. Soru: Verilen bir dizi içindeki tek sayıları bulup yeni bir diziye ekleyin.
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+
+// 2. Soru: Bir dizi içindeki sayıları büyükten küçüğe sıralamak için hangi dizi yöntemini kullanırsınız ve nasıl yaparsınız?
+
+// let numbers = [8, 4, 6, 2, 9, 5, 7];
+
+
+
+
+// 3. Soru: Bir dizi içindeki metin öğelerini birleştirerek tek bir metin oluşturmak için hangi dizi yöntemini ve diziyi nasıl kullanırsınız?
+
+// let words = ["Merhaba", "dünya", "bu", "bir", "örnektir"];
+
+
+
+
+// 4. Soru: Bir cümleyi alın ve kelimeleri ters sırayla birleştirerek yeni bir cümle oluşturun.
+
+
+// let sentence = "JavaScript harika bir dil";
+
+
+
+// 5. Soru: Bir metinde "u" harfini içeren kaç kelime olduğunu bulmak için hangi dizi yöntemini kullanırsınız ve nasıl yaparsınız?
+
+// let text = "There are many shorthand techniques in JavaScript that can shorten the code length, reduce redundancy, and improve the readability and maintainability of the code. This article will introduce 20 JS abbreviation techniques to improve efficiency, helping you say goodbye to the shit mountain and write elegant code easily!";
+
+
+
+// 6. Soru: İki dizi içindeki ortak elemanları bulup yeni bir dizi oluşturun?
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = [3, 4, 5, 6, 7];
+
+
+// 7. Bir isim listesindeki isimlerden ilk 10 ve son 10 ismi çıkartarak yeni bir sıralı liste oluşturun.
+
+// let isimListesi = ["Ali", "Ayşe", "Mehmet", "Fatma", "Hasan", "Zeynep", "Emre", "Selin", "Mustafa", "Elif", "Baran", "Gamze", "Eren", "Dilara", "Burak", "Pınar", "İsmail", "Sibel", "Murat", "Gülşah", "Ahmet", "Hatice", "Kemal", "Sevgi", "Yusuf", "Aslı", "Can", "Zehra", "Onur", "Ebru", "Serkan", "Nazlı", "Cemal", "Gonca", "Ercan", "Esra", "Hakan", "Nur"];
