55d364c07dc35b88a5d3093cc5267731c5e8a2a4
hasan <hasancilenger1773@gmail.com>
2023-11-02 11:29:39 +0100

    queryselector

--- javascript/class-notes/js12_dom-intro/selectors.js
+++ javascript/class-notes/js12_dom-intro/selectors.js
@@ -30,7 +30,7 @@ h2.textContent = "ADD"
 h2.innerText = "ADD ITEM"
 h2.innerHTML = "<p>Deneme</p>"
 
-h2.innerHTML = "<p style='color:red'>Deneme</p>"
+h2.innerHTML = "<p>Deneme</p>"
 
 //* Input elemanlarinin degeri value property'si ile degisir
 document.getElementById("btn").value = "Submit"
@@ -55,17 +55,62 @@ for (let li of lists) {
   console.log(li.textContent)
 }
 //? HTML Collection yapısından Array cevrilebilir.
-const listArray = [...lists] //? array e cevrildi.
+const listArray = [...lists] //? Spread ile array e cevrildi.
 listArray.forEach((li) => (li.style.color = "green"))
 
+//? Array.from() metodu ile yine Array'e cevrim yapılabilir.
+Array.from(lists).forEach((li) => (li.style.backgroundColor = "yellow"))
+
+//? ORNEK
+const sections = document.getElementsByTagName("section")
+console.log(sections) //? HTMLCollection(2) [section.add-item, section.item-list]
+
+sections[0].style.backgroundColor = "blue"
+sections[0].style.color = "yellow"
+
+sections[1].style.color = "blue"
 //*===========================================
 //*          GETELEMENTSBYCLASSNAME()
 //*===========================================
 
+const myLists = document.getElementsByClassName("list")
+console.log(myLists) //?HTMLCollection(5) [li.list, li.list, li.list, li.list, li.list]
+
+console.log(myLists[3].innerText)
+
+const section = document.getElementsByClassName("item-list") //? tek elemanli bir dizi
+console.log(section[0])
+
 //* ========================================
 //*              QUERYSELECTOR()
 //* ========================================
+// ! Query Selector ile id, tag, class seçilebilir.
+//! bu secici akısta gördügü ilk elementi secer.
 
+//? id almak için (#)
+console.log(document.querySelector("#header"))
+
+//? class almak için (.)
+console.log(document.querySelector(".item-list"))
+
+//? tag almak için ()
+console.log(document.querySelector("h3"))
+
+//? CSS selector
+console.log(document.querySelector("section.add-item #btn"))
+
+console.log(document.querySelector("ul li:nth-child(4)").innerText)
 //* ========================================
 //*              QUERYSELECTORALL()
 //* ========================================
+const itemLists = document.querySelectorAll("section.item-list li")
+console.log(itemLists) //? NodeList(5) [li.list, li.list, li.list, li.list, li.list]
+
+//* querySelectorAll bir nodelist dondurur. Nodelist dahali olarak forEach metmodunu barindirir. Ama istenirse spread veya Array.from() ile yine Array'e donusum yapilabilir.
+
+itemLists.forEach((li) => console.log(li))
+
+//? alternatif iterasyon
+for (let li of itemLists) {
+  console.log(li)
+}
